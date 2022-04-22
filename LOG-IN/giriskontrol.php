085ba1640a35447b62257d8be54c8c8a4e7f098a
aleyna kara <92330034+aleynakara16@users.noreply.github.com>
2022-04-22 08:46:44 +0300

    LOG-IN

---
+++ LOG-IN/giriskontrol.php
@@ -0,0 +1,23 @@
+<?php
+$adsoyad =$_POST["Username"];
+$sifre =$_POST["Password"];
+if($adsoyad && $sifre) // veriler boş değilse buradan devam eder
+{
+    if($adsoyad == "b211210004@sakarya.edu.tr" && $sifre=="b211210004")
+    {  
+        echo "hoşgeldin". $adsoyad;
+
+       header("location:ANASAYFA.html"); 
+    }
+    else
+    {
+        header("location:giriş.html"); 
+
+    }
+} 
+else 
+{
+header("location:giris.html"); // veriler boşsa sayfaya geri gönderir
+}
+
+?>
