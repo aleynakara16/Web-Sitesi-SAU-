7449652f165905076bb4e540cc72d9ed3952aa7e
aleyna kara <92330034+aleynakara16@users.noreply.github.com>
2022-05-10 09:49:42 +0300

    son kontroller
    hataları düzelttim ve stil ile biraz oynadım

--- giriskontrol.php
+++ giriskontrol.php
@@ -1,39 +1,36 @@
-
-
-
-
 <html>
     <head>
 
     </head>
 
-    <body style="background-color:darkgray;">
-    <div class="container" style="margin: auto;border:1px;">
-    <p><?php
-$adsoyad =$_POST["Username"];
-$sifre =$_POST["Password"];
-if($adsoyad && $sifre) // veriler boş değilse buradan devam eder
-{
-    if($adsoyad == "b211210004@sakarya.edu.tr" && $sifre=="b211210004")
-    {  
-        echo "HOŞGELDİN      ". $adsoyad;
+<body style="background-color:darkgray;">
+ <div >
+   <p>
+       <?php
+         $adsoyad =$_POST["Username"];
+         $sifre =$_POST["Password"];
+         if($adsoyad && $sifre) // veriler boş değilse buradan devam eder
+         {
+            if($adsoyad == "b211210004@sakarya.edu.tr" && $sifre=="b211210004")
+            {  
+                echo "HOŞGELDİN ". $adsoyad;
+        
+            }
+            else
+            {
+                header("location:giriş.html"); 
+        
+            }
+         } 
+         else 
+         {
+          header("location:giris.html"); // veriler boşsa sayfaya geri gönderir
+         }
+       ?>
+   </p>
 
-    }
-    else
-    {
-        header("location:giriş.html"); 
-
-    }
-} 
-else 
-{
-header("location:giris.html"); // veriler boşsa sayfaya geri gönderir
-}
-
-?></p>
-
-    </div>
+ </div>
 
 
-    </body>
+</body>
 </html>
